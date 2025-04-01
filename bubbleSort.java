fc66aa485219366b882209c515d79a4a786847c6
Tangerinn <tanayapawar248@gmail.com>
2025-04-01 22:21:05 +0530

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Sort {
+
+    static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (j + 1 <= arr.length - 1 && arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 11, 10, 4, 30, -1, -2, -4, 0, 7, 38 };
+        int newArray[] = bubbleSort(arr);
+        printArray(newArray);
+
+    }
+}
