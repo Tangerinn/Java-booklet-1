818394dffa8c923ba6c810315ecb7e010e4c4fc2
Tangerinn <tanayapawar248@gmail.com>
2025-09-23 12:55:44 +0530

    Update HelloWorld.java
    change 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
     public static void main(String[] args) 
 {
         System.out.println("Hello, World!");
+        System.out.println("Change 1. Checking if jenkins will automatically compile and run this file once I commit this change.");
     }
 }
